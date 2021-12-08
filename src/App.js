b8eba53437e75559bcaec87fd89a8a7813b256b0
Tomsav83 <tomsav83@gmail.com>
2021-12-08 11:05:45 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    error: false,
+    message: '',
+  });
+  const getter = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch('https://api.adviceslip.com/advice');
+      if (response.status !== 200) {
+        throw new Error('THE API IS DOWN');
+      }
+      const data = await response.json();
+      setAdvice(data.slip);
+      console.log(data);
+      setLoading(false);
+    } catch (error) {
+      setError({ error: true, message: error.message });
+    }
+  };
+
+  useEffect(() => {
+    getter();
+  }, []);
+
+  if (error.error) {
+    return <h1>{error.message}</h1>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello Im app</h1>
+      {loading ? <p>loading...</p> : <h2>{advice.advice}</h2>}
+      <button onClick={getter}>get data</button>
     </div>
   );
-}
+};
 
 export default App;
